4bb8b0e1a89bf7d6433cb31a6b26a9b9c33109da
fabien ladouce <ladouce.fabien@gmail.com>
2018-02-08 13:23:31 +0100

    Récupération des champs d'intervention + création d'une intervetion

--- Bubuche/app/src/main/java/com/example/aurelienthazet/bubuche/CreationInterventionActivity.java
+++ Bubuche/app/src/main/java/com/example/aurelienthazet/bubuche/CreationInterventionActivity.java
@@ -6,10 +6,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import bdd.InterventionDAO;
+import metier.Intervention;
 
 public class CreationInterventionActivity extends AppCompatActivity {
 
     private Button btAccueilCreerIntervention;
+
+    private TextView tvCreationInterventionIdArbre;
+    private EditText ptTypeIntervention;
+    private EditText ptDateCreationIntervention;
+    private EditText ptHeureCreationIntervention;
+    private EditText mtObsCreationIntervention;
+
+
+    //Valeur saisies
+    int idArbre;
+    String typeIntervention;
+    String dateCreationIntervention;
+    String heureCreationIntervention;
+    String observations;
+
     @SuppressLint("WrongViewCast")
 
     @Override
@@ -25,5 +45,27 @@ public class CreationInterventionActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });*/
+    }
+
+    public void gererCreation(){
+        idArbre = Integer.parseInt(getIntent().getStringExtra("selected-item"));
+
+        ptTypeIntervention = (EditText) findViewById(R.id.ptTypeIntervention);
+        ptDateCreationIntervention = (EditText) findViewById(R.id.ptDateCreationIntervention);
+        ptHeureCreationIntervention = (EditText) findViewById(R.id.ptHeureCreationIntervention);
+        mtObsCreationIntervention = (EditText) findViewById(R.id.mtObsCreationIntervention);
+
+
+        //METTRE CA DANS UN ONCLICK LISTENER
+        typeIntervention = ptTypeIntervention.getText().toString();
+        dateCreationIntervention = ptDateCreationIntervention.getText().toString();
+        heureCreationIntervention = ptHeureCreationIntervention.getText().toString();
+        observations = mtObsCreationIntervention.getText().toString();
+
+        Intervention uneIntervention = new Intervention(idArbre,dateCreationIntervention, heureCreationIntervention, observations);
+
+        InterventionDAO uneInterventionDAO = new InterventionDAO(getApplicationContext());
+
+        uneInterventionDAO.create(uneIntervention);
     }
 }
